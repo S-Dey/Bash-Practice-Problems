c8220571362b8950f3dec79129c2513665d7a3c4
Subhadeep Dey <contact.sdey@gmail.com>
2017-12-13 14:53:59 +0530

    Bug fixes.

--- Bash/p3.sh
+++ Bash/p3.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 #P3: Write a shell script to create a file and a directory wherein the first argument passed in the terminal is used for creating the file and second one is used for creating the directory.
 
-if [ ! -f "$1" ]; then
-    touch "$1";
-    echo "Successfully create file '$1' in $PWD";
-else 
-    echo "The file already exists in the current directory.";
+if [ -f "$1" ]; then 
+    echo "The file '$1' already exists in the working directory.";
+elif touch "$1"; then
+    echo "Successfuly created the file '$1' in '$PWD'.";
 fi
 
-if [ ! -d "$2" ]; then
-    mkdir "$2";
-    echo "Successfully created directory '$2' in $PWD";
-else 
-    echo "The entered directory already exists in the current directory."
+if mkdir "$2"; then
+    echo "Successfully created directory '$2' inside the working directory!";
 fi
