ba6533893b105060d4fe091c9006e6c3108d102f
Subhadeep Dey <contact.sdey@gmail.com>
2017-12-13 13:50:12 +0530

    Bug fixes.

--- Bash/p1.sh
+++ Bash/p1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #P1: Write a shell script to check whether the name passed as the first argument is the name of an existing file or directory.
 
-if [ -f $1 ]; then
+if [ -f "$1" ]; then
     echo "It is a file.";
-elif [ -d $1 ]; then  
+elif [ -d "$1" ]; then  
     echo "It is a directory.";
 else 
     echo "It is neither a file nor a directory.";
