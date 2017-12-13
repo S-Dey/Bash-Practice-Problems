dff73f4df1e9b326bc8d5e607197cefeabdbbe56
Subhadeep Dey <contact.sdey@gmail.com>
2017-12-13 23:36:29 +0530

    Fixes

--- Bash/p15.sh
+++ Bash/p15.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 #P15: Write a shell script to input a string. If its length is lesser than 6 characters, then print “Name too short!”.
 
-
-#Using expr
 echo "Enter any string: "
 read string
 
-length=`expr "$string" : ".*"`
+length=$(expr "$string" : ".*");
 
-if [ $length -le 6 ]; then
-    echo "Length is shorter than 6."
+if [ "$length" -le 6 ]; then
+    echo "Good! Length is shorter than 6.";
 else
-    echo "Error."
+    echo "Bad! The lenght should not be greater than 6!";
 fi
