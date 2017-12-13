4807606d18e76ef40aac54ae1112a563d6b9aa4d
Subhadeep Dey <contact.sdey@gmail.com>
2017-12-14 03:08:04 +0530

    Bug fixes.

--- Bash/p17.sh
+++ Bash/p17.sh
@@ -2,7 +2,7 @@
 #P17: Write a shell script to return the last modification time of a file whose name is passed as an argument in the terminal. 
 
 if [ -f "$1" ]; then
-    set -- $(ls -l "$1");
+    set -- "$(ls -l "$1")";
     echo "$0 $1 $2 $3 $4 $5 $6 $7 $8 $9";
 else
     echo "The entered file does not exist.";
