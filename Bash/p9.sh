66b96b374687dc6f6a5b993ff49c9e0ee0c2f2c9
Subhadeep Dey <contact.sdey@gmail.com>
2017-12-13 22:59:21 +0530

    Added p9

--- Bash/p9.sh
+++ Bash/p9.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-while read -r a b; do
-    echo $a $b
-done < text.txt
+#P9: Write a shell script in which a list of filenames is taken as the input. If the file found in the list is called “hello”, then break the condition and print the index of its occurrence in the list.
+
+index=1;
+
+echo "The file 'hello' is found at argument index(es):"
+for i in "$@"
+do
+    if [[ "$i" == "hello" || "$i" == "Hello" ]]; then
+        echo "$index";
+    fi
+
+    ((index++));
+    
+done
